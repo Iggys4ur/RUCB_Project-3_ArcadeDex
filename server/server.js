d7a7f694042476a088335db8ab7c71fe6112e29a
Jay <jayden_benston@yahoo.com>
2024-08-29 14:00:26 -0400

    Steam auth made

--- server/server.js
+++ server/server.js
@@ -5,6 +5,12 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const cookieParser = require('cookie-parser');
 const { verify } = require('jsonwebtoken');
 const path = require('path');
+const session = require('express-session');
+
+const passport = require('passport');
+const SteamStrategy = require('passport-steam').Strategy;
+const MongoStore = require('connect-mongo');
+const mongooseConnection = require('./config/connection');
 
 const typeDefs = require('./schema/typeDefs');
 const resolvers = require('./schema/resolvers');
@@ -13,7 +19,20 @@ const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const dbURL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/ArcadeDex';
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: dbURL,
+    collectionName: 'sessions'
+  })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs,
@@ -50,6 +69,45 @@ async function startServer() {
       }
     })
   );
+
+  // Configure passport-steam
+  passport.use(new SteamStrategy({
+    returnURL: 'http://localhost:3001/auth/steam/return',
+    realm: 'http://localhost:3001/',
+    apiKey: process.env.STEAM_KEY,
+  }, (identifier, profile, done) => {
+    // In a real application, you would save the user profile to your database here
+    return done(null, profile);
+  }));
+  // Serialize user into the session
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  // Deserialize user from the session
+  passport.deserializeUser((obj, done) => {
+    done(null, obj);
+  });
+
+  app.get('/', (req, res) => {
+    console.log(req.user);
+    res.send(req.isAuthenticated() ? req.user.displayName : 'Logged out');
+  });
+
+  app.get('/auth/steam',
+    passport.authenticate('steam', { failureRedirect: '/' }),
+    (req, res) => {
+      // The request will be redirected to Steam for authentication, so this
+      // function will not be called.
+    });
+  app.get('/auth/steam/return',
+    passport.authenticate('steam', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/');
+    });
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
 
   if (process.env.PORT) {
     app.use(express.static('../client/dist'));
