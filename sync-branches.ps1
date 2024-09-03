ca8a41dfe838bbdcd0b57892d0784fc70916edaa
Iggys4ur <eswaind@gmail.com>
2024-09-03 11:57:30 -0400

    fixed sync-branches.ps1 (again x3)

--- sync-branches.ps1
+++ sync-branches.ps1
@@ -6,20 +6,20 @@
  
   # Identify remote branches not existing locally
   $remoteOnlyBranches = $remoteBranches | Where-Object { $_ -notin $localBranches }
-  Write-Output "Remote branches not existing locally: $remoteOnlyBranches \n\n"
+  Write-Output "Remote branches not existing locally: $remoteOnlyBranches `n`n"
  
   # Identify local branches not existing remotely
   $localOnlyBranches = $localBranches | Where-Object { $_ -notin $remoteBranches }
-  Write-Output "Local branches not existing remotely: $localOnlyBranches \n\n"
+  Write-Output "Local branches not existing remotely: $localOnlyBranches `n`n"
  
   # Pull remote-only branches to local repository
   foreach ($branch in $remoteOnlyBranches) {
       git checkout -b $branch origin/$branch
-          Write-Output "*****\nPulled remote branch: $branch \n"
+          Write-Output "*****`nPulled remote branch: $branch `n"
           }
  
   # Push local-only branches to GitHub
   foreach ($branch in $localOnlyBranches) {
       git push origin $branch
-          Write-Output "*****\nPushed local branch: $branch"
+          Write-Output "*****`nPushed local branch: $branch"
           }
