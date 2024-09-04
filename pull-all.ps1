7127470d48b5a098803c79cc5f344bd7d2147711
Iggys4ur <eswaind@gmail.com>
2024-09-03 23:55:29 -0400

    added git-pull-all.ps1 to [...]/MISC/useful-scripts/git-pull-all.ps1 to run `git pull origin <branch-name>` on every local branch (`git branch`)

---
+++ pull-all.ps1
@@ -0,0 +1,14 @@
+# Get the list of branches
+$branches = git branch
+
+# Iterate over each branch and perform checkout and pull
+foreach ($branch in $branches) {
+    # Trim any whitespace and '*' characters that might be present
+    $branch = $branch.Trim().TrimStart('*').Trim()
+
+    Write-Output "Switching to branch: $branch"
+    git checkout $branch
+
+    Write-Output "Pulling latest changes for branch: $branch"
+    git pull origin $branch
+}
